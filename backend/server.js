370d83695b1c98109b6a5519a8fa65c0c60a8bf7
Michael Gomes <michaelgomesweb@gmail.com>
2022-07-15 18:34:35 -0400

    Dotenv & Connect To Database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
+// Setup Dotenv, Express & JSON Parsing
+dotenv.config();
 const app = express();
+app.use(express.json());
 
-const PORT = 5000;
+// Connect To Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
